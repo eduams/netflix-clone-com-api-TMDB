9f0d51f6b004909f53626b906e3f970d8cdc779f
eduams1 <eduardo4ms@gmail.com>
2023-06-13 03:55:51 -0300

    Video do header

--- js/main.js
+++ js/main.js
@@ -12,3 +12,31 @@ window.onscroll = function() {
   }
   
 console.log(resolutionWidth)
+
+
+window.onload = function playonstart(){
+  setTimeout(function(){
+    let header = document.getElementById("topimg");
+    header.style.opacity = "1";
+    header.remove();
+    let headerdiv = document.getElementById("header-media");
+    let youtubeIframe = document.createElement("iframe");
+
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMDNiY2FkNzNhNWMzMTQwMjQxZjg5MDkzNjQ2MDk4MSIsInN1YiI6IjY0N2I2OWM2Y2FlZjJkMDEzNjJiMWJmZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nHBWCz8ipknMv7An_AAUbe4v5uqoQxHznpZTX0SF78w'
+      }
+    };
+    
+    fetch('https://api.themoviedb.org/3/tv/45790/videos?language=en-US', options)
+      .then(response => response.json())
+      .then(response => {
+        youtubeIframe.setAttribute("src", "https://www.youtube.com/embed/"+response.results[0].key+"?autoplay=1&mute=1");
+        youtubeIframe.setAttribute("id","topimg");
+        headerdiv.appendChild(youtubeIframe);
+      })
+      .catch(err => console.error(err));  
+  },1000); 
+}
